a5475b09759c256c14f855e0398b1f467940be07
han <wuuuuhan@outlook.com>
2022-03-17 09:22:45 +0800

    更新Windows端安装脚本

---
+++ usbip/install.cmd
@@ -0,0 +1,22 @@
+@echo off
+pushd "%~dp0" && net sess 1>nul 2>nul || (powershell -ex unrestricted -Command "Start-Process -Verb RunAs -FilePath '%comspec%' -ArgumentList '/c \"%~f0\" %*'" >nul 2>nul & exit /b 1)
+
+
+set _CONF_FILE="%~dp0%_CONF%"
+set _CONF_NEW="%~dp0%_CONF%_new"
+set _SVCCTL="%~dp0nssm.exe"
+set _USBIP_EXE="%~dp0usbip.exe"
+set _USBIP_CERT="%~dp0usbip_test.pfx"
+set _CERTMGR="%~dp0certmgr.exe"
+
+@REM @REM 启动测试模式
+bcdedit.exe /set TESTSIGNING ON
+
+@REM 安装证书
+
+@REM %_CERTMGR% /add /all %_USBIP_CERT% /s /r localMachine Personal
+%_CERTMGR% /add /all %_USBIP_CERT% /s /r localMachine AuthRoot
+%_CERTMGR% /add /all %_USBIP_CERT% /s /r localMachine TRUSTEDPUBLISHER
+
+@REM @REM 安装驱动
+%_USBIP_EXE% install
