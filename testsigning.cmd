f0411dff94b5af05eeb61a3a2acde3d0b3850688
han <wuuuuhan@outlook.com>
2022-03-24 10:02:25 +0800

    删除一个放错位置的文件

--- testsigning.cmd
+++
@@ -1,8 +0,0 @@
-
-@echo off
-pushd "%~dp0" && net sess 1>nul 2>nul || (powershell -ex unrestricted -Command "Start-Process -Verb RunAs -FilePath '%comspec%' -ArgumentList '/c \"%~f0\" %*'" >nul 2>nul & exit /b 1)
-
-
-
-@REM @REM 启动测试模式
-bcdedit.exe /set TESTSIGNING ON
